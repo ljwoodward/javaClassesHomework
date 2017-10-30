c5fcfa674cf4d46cc3c1d4677421c9c2ea77608d
Laurence <ljwoodward.code@gmail.com>
2017-10-30 18:54:09 +0000

    created ClassroomTest file and first test

---
+++ Classroom.java
@@ -0,0 +1,10 @@
+class Classroom {
+private String name;
+// private Student[] students;
+
+public Classroom( String name ) {
+  this.name = name;
+  // this.students = new Student[20]
+  }
+
+}
---
+++ ClassroomTest.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class ClassroomTest {
+
+  Classroom classroom;
+
+  @Before
+  public void before() {
+    this.classroom = new Classroom( "E16" );
+  }
+
+  @Test
+  public String hasName() {
+    String name = this.classroom.getName();
+    assertEquals( "E16", name);
+  }
+}
